281a3bd0ee773591a27a8e58d5835d49a3ae4978
Samuel Lucio Garcia Hidalgo <samuelluciogar@gmail.com>
2024-03-24 20:09:42 -0400

    fix wrong casing

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Arsenal:ital,wght@0,400;0,700;1,400;1,700&family=Marck+Script&display=swap"
           rel="stylesheet"
